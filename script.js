5a748dbd5342632f310a6ed6687e4d2fb14fc6f2
“Matthew8991” <matthewmmclanahan@gmail.com>
2022-03-02 09:58:12 -0500

    Created VariablesExercise folder and script.js file.

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1)
+// const is used for variables that will not change
+
+// 2)
+let num = 20;
+
+// 3)
+num = 15;
+
+// 4)
+num = num + 7;
+
+// 5)
+num--;
+
+// 6)
+num = "This is not a number";
+
+// 7)
+let blank;
+
+// 8)
+console.log(blank);
+
+// 9)
+blank = null;
+
+// Bonus 1)
+const [quarter, dime, nickle, penny] = [0.25, 0.10, 0.5, 0.1];
+
+// Bonus 2)
+console.log(quarter, dime, nickle, penny);
+
+// Bonus 3)
+let letters = "qwertyuiop";
+
+// Bonus 4)
+console.log(letters.length);
+console.log(letters[9]);
